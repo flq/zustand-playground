be623f7e55ffec714f11c93253bf9174a9cfa9eb
frank <frank@aheadintranet.com>
2022-08-19 21:34:10 +0200

    some cosmetics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import styles from "./App.module.css";
 import { HeaderDisplay } from "./HeaderDisplay";
+import { Items } from "./Items";
 
 function App() {
   return (
     <div className={styles.app}>
       <HeaderDisplay />
+      <Items />
     </div>
   );
 }
