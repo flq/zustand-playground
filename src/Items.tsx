dc5436e3b86c1c688a30a48f258443bde6367099
frank <frank@aheadintranet.com>
2022-08-21 18:16:45 +0200

    modified selectors + comparers

--- src/Items.tsx
+++ src/Items.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 import { useAppStore } from "./AppStore";
 import styles from "./App.module.css";
-import { Data } from "./data";
 
 export function Items() {
-  const items = useAppStore(s => s.items);
+  const items = useAppStore(
+    (s) => s.items,
+    (prev, curr) => prev.length === curr.length
+  );
   return (
     <ul className={styles.items}>
-      {items.map((item, index) => (
-        <Item key={index} index={index} item={item} />
+      {items.map((_, index) => (
+        <Item key={index} index={index} />
       ))}
     </ul>
   );
 }
 
-function Item({ index, item }: { index: number; item: Data["items"][number] }) {
-  const toggleCheckedItemAt = useAppStore(s => s.toggleCheckedItemAt);
+function Item({ index }: { index: number }) {
+  const { toggleCheckedItemAt, item } = useAppStore(
+    ({ toggleCheckedItemAt, items: { [index]: item } }) => ({
+      toggleCheckedItemAt,
+      item,
+    }),
+    ({ item: prev }, { item: curr }) => prev === curr
+  );
   return (
     <li className={styles.item}>
       <h3>{item.text}</h3>
